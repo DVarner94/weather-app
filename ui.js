a0f91a9e3ba44da7911d02b51c0a3432729d226a
Dominic Varner <dommy@MLBs-Lappy.local>
2020-10-27 12:52:03 -0700

    setting info from API to the DOM

--- ui.js
+++ ui.js
@@ -9,4 +9,13 @@ class UI{
         this.dewpoint = document.getElementById('w-dewpoint')
         this.wind = document.getElementById('w-wind')
     }
+    paint(weather){
+        this.location.textContent = weather.name
+        this.desc.textContent = weather.weather[0].description
+        this.string.textContent = weather.main.temp + 'F'
+        this.icon.setAttribute('src', `http://openweathermap.org/img/wn${weather.weather[0]}@2x.png`)
+        this.humidity.textContent = `Relative Humidity: ${weather.main.humidity}`
+        this.dewpoint.textContent = `Wind Direction: ${weather.wind.deg} degrees`
+        this.wind.textContent. = `Wind Speed: ${weather.wind.speed}meters/sec`
+    }
 }

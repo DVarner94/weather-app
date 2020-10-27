466608daaa7f380606a97245463bb3d4724ef79d
Dominic Varner <dommy@MLBs-Lappy.local>
2020-10-27 12:46:51 -0700

    found bug, out of place {}

--- weather.js
+++ weather.js
@@ -1,22 +1,22 @@
 class Weather {
-    constructor(city, state){
+    constructor(city, state) {
         this.apiKey = '393ecd597caf6bb97d30507eafc61b82';
         this.city = city;
         this.state = state;
     }
-}
 
-// fetch weather from api
-async getWeather(){
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&APPID=${this.apiKey}`);
+    // fetch weather from api
+    async getWeather() {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&APPID=${this.apiKey}`);
 
 
-    const responseData = await response.json();
+        const responseData = await response.json();
 
-    return responseData.current_observation
+        return responseData.current_observation;
+    }
 
     // change location
-    changeLocation(city, state){
+    changeLocation(city, state) {
         this.city = city;
         this.state = state;
     }

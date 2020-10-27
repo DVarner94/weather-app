92bc24d786917a17fe6cbd3ddf81b0860ec5f41d
Dominic Varner <dommy@MLBs-Lappy.local>
2020-10-27 12:59:18 -0700

    grab stored data

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+const storage = new Storage()
+
+// get stored location data
+const weatherLocation = storage.getLocationData()
+
 // init weather object
-const weather = new Weather(weatherLocation.city, weatherLocation.this.state.);
+const weather = new Weather(weatherLocation.city, weatherLocation.this.state);
 
 weather.getWeather()
     .then(results => {

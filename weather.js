872136077c6f71b3338d7e8d9c4b93bc8c26867e
Dominic Varner <dommy@MLBs-Lappy.local>
2020-10-27 12:25:23 -0700

    weather fetch + change location

--- weather.js
+++ weather.js
@@ -1,7 +1,23 @@
 class Weather {
     constructor(city, state){
-        this.apiKey = '393ecd597caf6bb97d30507eafc61b82',
-        this.city = city,
-        this.state = state
+        this.apiKey = '393ecd597caf6bb97d30507eafc61b82';
+        this.city = city;
+        this.state = state;
+    }
+}
+
+// fetch weather from api
+async getWeather(){
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&APPID=${this.apiKey}`);
+
+
+    const responseData = await response.json();
+
+    return responseData.current_observation
+
+    // change location
+    changeLocation(city, state){
+        this.city = city;
+        this.state = state;
     }
 }
